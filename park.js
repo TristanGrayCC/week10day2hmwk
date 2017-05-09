429e1cc126e4183cccb0af4c1701420cc32904cc
Tristan Gray <tristangraycc@gmail.com>
2017-05-09 17:23:45 +0100

    Completed basics

--- park.js
+++ park.js
@@ -5,6 +5,25 @@ var Park = function(){
 Park.prototype ={
   add: function(dinosaur){
     this.enclosure.push(dinosaur);
+  },
+
+  removeType: function(findType){
+    for(i=this.enclosure.length-1; i>=0; i--) {
+      if(this.enclosure[i].type === findType) {
+        this.enclosure.splice(i,1)
+      };
+    };
+  },
+
+  collecteBreeders: function(findType){
+    var breeders = [];
+    for(dinosaur of this.enclosure) {
+      if(dinosaur.noOfOffspring > 2) {
+        breeders.push(dinosaur);
+      }
+    }
+    console.log("Breed: ", breeders);
+    return breeders;
   }
 }
 
--- specs/park_specs.js
+++ specs/park_specs.js
@@ -17,4 +17,34 @@ describe("Park", function(){
     park.add(dinosaur2);
     assert.equal(2, park.enclosure.length);
   });
+
+  it("Can remove T Rexes",function(){
+    var park = new Park();
+    var dinosaur1 = new Dinosaur("Tyrannosaurus Rex", 2);
+    var dinosaur2 = new Dinosaur("Tyrannosaurus Rex", 2);
+    var dinosaur3 = new Dinosaur("Tyrannosaurus Rex", 2);
+    var dinosaur4 = new Dinosaur("Tricerotops", 4);
+    park.add(dinosaur1);
+    park.add(dinosaur2);
+    park.add(dinosaur3);
+    park.add(dinosaur4);
+    park.removeType("Tyrannosaurus Rex");
+    assert.equal(1, park.enclosure.length);
+  });
+
+  it("Can get high breeders",function(){
+    var park = new Park();
+    var dinosaur1 = new Dinosaur("Tyrannosaurus Rex", 2);
+    var dinosaur2 = new Dinosaur("Tyrannosaurus Rex", 2);
+    var dinosaur3 = new Dinosaur("Tyrannosaurus Rex", 2);
+    var dinosaur4 = new Dinosaur("Tricerotops", 4);
+    var dinosaur5 = new Dinosaur("Tricerotops", 4);
+    park.add(dinosaur1);
+    park.add(dinosaur2);
+    park.add(dinosaur3);
+    park.add(dinosaur4);
+    park.add(dinosaur5);
+    var breeders = park.collecteBreeders().length;
+    assert.equal(2, breeders);
+  });
 });
