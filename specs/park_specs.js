c69c98c8b14d0cdb7c22419dcd5351414959f88c
Tristan Gray <tristangraycc@gmail.com>
2017-05-09 16:58:12 +0100

    Adding dinosaurs

--- specs/park_specs.js
+++ specs/park_specs.js
@@ -8,4 +8,13 @@ describe("Park", function(){
     var park = new Park();
     assert.equal(0, park.enclosure.length);
   });
+
+  it("Can add Dinosaurs",function(){
+    var park = new Park();
+    var dinosaur1 = new Dinosaur("Tyrannosaurus Rex", 2);
+    var dinosaur2 = new Dinosaur("Tricerotops", 4);
+    park.add(dinosaur1);
+    park.add(dinosaur2);
+    assert.equal(2, park.enclosure.length);
+  });
 });
